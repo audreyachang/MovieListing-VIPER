eb0d1d2ff3daff101b0f2d41f076b208f4fbbcda
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2022-09-06 20:17:10 +0700

    Added review cell
    Added review cell file to connect UI elements on Review Cell

---
+++ MovieListing-VIPER/Presenter/ReviewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReviewCell.swift
+//  MovieListing-VIPER
+//
+//  Created by Audrey Aurelia Chang on 06/09/22.
+//
+
+import UIKit
+
+class ReviewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
